5ce5f9954a1f8e15ac15faeb4e4518a467d62bdd
mlhazan <mlhazan@hotmail.com>
2018-01-25 01:05:24 -0500

    webflow added

--- src/main/java/com/packt/webstore/service/impl/OrderServiceImpl.java
+++ src/main/java/com/packt/webstore/service/impl/OrderServiceImpl.java
@@ -2,17 +2,25 @@ package com.packt.webstore.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.packt.webstore.domain.Order;
 import com.packt.webstore.domain.Product;
+import com.packt.webstore.domain.repository.OrderRepository;
 import com.packt.webstore.domain.repository.ProductRepository;
+import com.packt.webstore.service.CartService;
 import com.packt.webstore.service.OrderService;
 
 @Service
 public class OrderServiceImpl implements OrderService {
 	@Autowired
 	private ProductRepository productRepository;
+	@Autowired
+	private OrderRepository orderRepository;
+	@Autowired
+	private CartService cartService;
 
 	// private ProductRepository productRepository= new InMemoryProductRepository();
-	public void processOrder(String productId, int quantity) {
+	public void processOrder(String productId, long quantity) {
 		Product productById = productRepository.getProductById(productId);
 		if (productById.getUnitsInStock() < quantity) {
 			throw new IllegalArgumentException(
@@ -20,4 +28,12 @@ public class OrderServiceImpl implements OrderService {
 		}
 		productById.setUnitsInStock(productById.getUnitsInStock() - quantity);
 	}
+
+	public Long saveOrder(Order order) {
+		Long orderId = orderRepository.saveOrder(order);
+		cartService.delete(order.getCart().getCartId());
+		return orderId;
+	}
+
+
 }
